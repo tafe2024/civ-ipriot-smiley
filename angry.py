0901a63c50eb72dc4b09327087881abce4a9b9d1
J377133 <J377133@tafe.wa.edu.au>
2024-11-04 22:24:50 +0800

    added angry.py

---
+++ angry.py
@@ -0,0 +1,46 @@
+import time
+from smiley import Smiley
+
+
+class Angry(Smiley):
+    """
+   Provides a Smiley with a happy expression
+    """
+    def __init__(self):
+        super().__init__(complexion=self.RED)
+
+        self.draw_mouth()
+        self.draw_eyes()
+
+    def complexion(self):
+        # Overrides default face colour with red.
+        return self.RED
+    
+    def draw_mouth(self):
+        """
+       Renders a mouth by blanking the pixels that form that object.
+        """
+        mouth = [50, 51, 52, 53]
+        for pixel in mouth:
+            self.pixels[pixel] = self.BLANK
+
+    def draw_eyes(self, wide_open=True):
+        """
+       Draws the eyes (open or closed) on the standard smiley.
+        :param wide_open (bool): eyes open or closed.
+        """
+        eyes = [10, 13, 18, 21]
+        for pixel in eyes:
+            self.pixels[pixel] = self.BLANK if wide_open else self.my_complexion
+
+    def blink(self, delay=0.25):
+        """
+       Blinks the smiley's eyes once
+        
+        :param delay: Delay between blinks (in seconds)
+        """
+        self.draw_eyes(wide_open=False)
+        self.show()
+        time.sleep(delay)
+        self.draw_eyes(wide_open=True)
+        self.show()
